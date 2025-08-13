9b6832e03f46acf9d207a46535e645e98e7abc23
Kumar Seenu <143558134+kumarseenu@users.noreply.github.com>
2025-08-13 15:23:41 -0400

    fixed chrome

--- src/test/java/stepDefinitions/LoginStep.java
+++ src/test/java/stepDefinitions/LoginStep.java
@@ -12,7 +12,7 @@ public class LoginStep {
 
     @Given("the user is on the login page")
     public void userOnLoginPage() {
-//        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
         driver = new ChromeDriver();
         driver.get("https://practicetestautomation.com/practice-test-login/");
     }
